debe2a306a0187ff0a8a062985b28933744444d9
KuyomieKurama <kersar.500@gmail.com>
2023-12-26 23:40:29 +0100

    Init WPF-Projekt

---
+++ Arbeitsnachweis Helper/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Arbeitsnachweis_Helper
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
