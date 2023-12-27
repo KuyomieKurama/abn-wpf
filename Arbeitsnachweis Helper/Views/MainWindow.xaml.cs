a3c0411c78e828456e680e182e22062a82f1f986
KuyomieKurama <kersar.500@gmail.com>
2023-12-28 00:27:31 +0100

    Farben fürs Layout müssen noch überarbeitet werden

--- Arbeitsnachweis Helper/Views/MainWindow.xaml.cs
+++ Arbeitsnachweis Helper/Views/MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
 
 namespace Arbeitsnachweis_Helper.Views
 {
@@ -19,6 +22,46 @@ namespace Arbeitsnachweis_Helper.Views
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hwnd,int wMsg,IntPtr wParam,IntPtr lParam);
+
+        private void controlBar_MouseLeftButton(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+
+        private void controlBar_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        private void closeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void minimizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void maxBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState != WindowState.Maximized)
+                this.WindowState = WindowState.Maximized;
+            else
+                this.WindowState = WindowState.Normal;
+        }
+
+        private void controlBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2 && this.WindowState == WindowState.Maximized)
+                this.WindowState = WindowState.Normal;
+            else if(e.ClickCount == 2 && this.WindowState == WindowState.Normal)
+                this.WindowState = WindowState.Maximized;
         }
     }
 }
