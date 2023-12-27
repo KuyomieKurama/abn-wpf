f0b859783ffea05f196a73345928e5e5245f0c91
KuyomieKurama <kersar.500@gmail.com>
2023-12-27 01:09:18 +0100

    Update Mögliches Layout der App

--- Arbeitsnachweis Helper/Views/MainWindow.xaml.cs
+++ Arbeitsnachweis Helper/Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Arbeitsnachweis_Helper
+namespace Arbeitsnachweis_Helper.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
